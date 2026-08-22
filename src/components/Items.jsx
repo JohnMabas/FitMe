46f5851ac0d271b71bee0dcc218e78062fd09abf
mabas <johnmabas1@gmail.com>
2026-08-22 02:49:05 +0100

    item page

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,40 +1,32 @@
-
 import { useState, useEffect } from "react";
 const Items = () => {
-    const[recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        async function getRecipes() {
-            try{
-                const res = await fetch('https://dummyjson.com/recipes');
-                const data = await res.json()
-                setRecipes(data.recipes)
-                console.log(data)
-            }catch (error){
-                console.log(error.message)
-            }
-        }
-            getRecipes()
+  useEffect(() => {
+    async function getRecipes() {
+      try {
+        const res = await fetch("https://dummyjson.com/recipes");
+        const data = await res.json();
+        setRecipes(data.recipes);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getRecipes();
+  }, []);
 
-}, [])
+  return (
+    <>
+      {recipes.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.name}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-    return(
-        <>
-        
-        {
-            recipes.map((item)=> {
-                return(
-                    <div key={item.id}>
-                        <p>{item.name}</p>
-
-                    </div>
-                )
-
-            })
-        }
-    
-        </>
-    )
-}
- 
 export default Items;
