9858d0aae13284d7aa3dea7f13a45615638550ab
mabas <johnmabas1@gmail.com>
2026-08-26 10:34:22 +0100

    add search function

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,6 +1,9 @@
 import NavBar from "./NavBar";
+import { FaPeopleGroup } from "react-icons/fa6";
+import hand from "/src/assets/image/hand.png";
 import { useSearchParams } from "react-router";
 import { useEffect, useState } from "react";
+import Footer from "./Footer";
 
 export default function SearchResults({ recipes }) {
   const [searchParams] = useSearchParams();
@@ -16,21 +19,55 @@ export default function SearchResults({ recipes }) {
       <section>
         <NavBar />
 
-        <section>
-          <h1>Search Results for "{query}"</h1>
+        <section className="m-8">
+          <div className="space-y-4">
+            <h1>Search Results for "{query}"</h1>
+            <div className="flex items-center gap-3">
+              <button className="bg-[#FC8019] p-2 w-24 rounded-2xl text-white">
+                Dishes
+              </button>
+              <button className="border border-[#FC8019] p-2 rounded-2xl w-40">
+                Restaurants
+              </button>
+            </div>
+          </div>
 
-          {results.length === 0 ? (
-            <p>No recipes found.</p>
-          ) : (
-            results.map((recipe) => (
-              <div key={recipe.id}>
-                <img src={recipe.image} alt={recipe.name} />
-                <h2>{recipe.name}</h2>
-              </div>
-            ))
-          )}
+          <div className="mt-13 grid grid-cols-5 gap-96">
+            {results.length === 0 ? (
+              <p>No recipes found.</p>
+            ) : (
+              results.map((recipe) => (
+                <div
+                  key={recipe.id}
+                  className="flex items-center gap-3 px-4 py-4 bg-gray-100  w-87.5"
+                >
+                  <img className="w-32 rounded-lg" src={recipe.image} alt={recipe.name} />
+                  <div className="">
+                    <h2 className="font-bold">{recipe.name}</h2>
+                    <span className="text-sm">{recipe.tags[1]}</span>
+                    <div className="flex items-center gap-10">
+                      <span className="flex items-center gap-2">
+                        <img src={hand} alt="" />
+
+                        {recipe.prepTimeMinutes}
+                      </span>
+
+                      <span className="flex items-center gap-1">
+                        <span className="flex items-center gap-2">
+                          <FaPeopleGroup className="shrink-0 text-[20px] text-amber-500" />
+                          {recipe.cookTimeMinutes}
+                          <p>Mins</p>
+                        </span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
         </section>
       </section>
+      <Footer/>
     </div>
   );
 }
